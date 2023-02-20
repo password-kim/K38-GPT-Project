4db15d26942aa59e77e8707e4b27cb70bce4c138
Olive You <cheeeedy@gmail.com>
2023-02-20 13:41:32 +0900

    Project Setting

--- K38-GPT/Program.cs
+++ K38-GPT/Program.cs
@@ -1,3 +1,43 @@
-﻿using Slack.Webhook;
+﻿using Newtonsoft.Json;
+using Slack.Webhook;
+using System.Text;
 
+private static async Task SendSlackMessage(string message)
+{
+    using (var client = new HttpClient())
+    {
+        var content = new StringContent(JsonConvert.SerializeObject(new
+        {
+            text = message
+        }), Encoding.UTF8, "application/json");
+
+        var result = await client.PostAsync("YOUR_SLACK_WEBHOOK_URL", content);
+    }
+}
+
+private static void ScheduleTask(Action task)
+{
+    var nextRun = GetNextRun();
+    var timer = new Timer(x =>
+    {
+        task.Invoke();
+        ScheduleTask(task);
+    }, null, nextRun - DateTime.Now, TimeSpan.FromMilliseconds(-1));
+}
+
+private static DateTime GetNextRun()
+{
+    var now = DateTime.Now;
+    var nextRun = new DateTime(now.Year, now.Month, now.Day, 21, 30, 0, DateTimeKind.Local);
+    if (nextRun < now)
+    {
+        nextRun = nextRun.AddDays(1);
+    }
+    while (nextRun.DayOfWeek == DayOfWeek.Saturday || nextRun.DayOfWeek == DayOfWeek.Sunday)
+    {
+        nextRun = nextRun.AddDays(1);
+    }
+
+    return nextRun;
+}
 
